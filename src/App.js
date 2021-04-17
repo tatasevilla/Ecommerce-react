60c5fbb11a6ce673cf7acbe79252dca607440e25
tatasevilla <tatasevilla2@hotmail.com>
2021-04-17 14:27:18 -0300

    creando repositorio remoto de e-commerce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+import  React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Cart from './components/Cart'
+import Details from './components/Details'
+import Default from './components/Default'
 
-function App() {
+export default class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/" component={ProductList} />
+        <Route component={Default} />
+      </Switch>
+    
+    </React.Fragment>
   );
+  }
 }
 
-export default App;
